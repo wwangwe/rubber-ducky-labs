630d61be42972b77dba76cbbd6d200c84ebe782a
wwangwe <wangwetymothy@gmail.com>
2023-04-03 17:21:29 +0300

    Setup initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Robber Ducky Labs!
+      </h1>
+    </>
+  )
+}
+
+export default App
