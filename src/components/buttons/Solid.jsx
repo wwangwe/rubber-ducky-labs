f0a7f339d24d1162ae22b047184bd8503187e366
wwangwe <wangwetymothy@gmail.com>
2023-04-12 18:13:44 +0300

    Fix background image on button

--- src/components/buttons/Solid.jsx
+++ src/components/buttons/Solid.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export const Solid = ({ children, alt = false }) => {
+    console.log(alt)
     return (
-        <button className={`bg-${ alt ? "primary" : "white"} rounded-full px-8 p-4 hover:bg-primary hover:opacity-90 transition-colors duration-300`}>
+        <button className={`${ alt ? "bg-primary" : "bg-white"} rounded-full px-8 py-4 hover:bg-primary hover:opacity-90 transition-colors duration-300`}>
             <div className="flex justify-between space-x-2 tracking-wide">
                 <h6>{children || "Solid"}</h6>
             </div>
