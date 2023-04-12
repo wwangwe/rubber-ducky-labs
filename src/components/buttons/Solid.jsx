b865625959832ad28242784999e9b41c9d327703
wwangwe <wangwetymothy@gmail.com>
2023-04-12 20:34:50 +0300

    Make button not rounded

--- src/components/buttons/Solid.jsx
+++ src/components/buttons/Solid.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Solid = ({ children, alt = false }) => {
     console.log(alt)
     return (
-        <button className={`${ alt ? "bg-primary" : "bg-white"} rounded-full px-8 py-4 hover:bg-primary hover:opacity-90 transition-colors duration-300`}>
+        <button className={`${ alt ? "bg-primary" : "bg-white"} px-8 py-4 hover:bg-primary hover:opacity-70 transition-colors duration-300`}>
             <div className="flex justify-between space-x-2 tracking-wide">
                 <h6>{children || "Solid"}</h6>
             </div>
