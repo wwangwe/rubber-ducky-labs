97185b71e1edadfacc1a95a0ef7a45e2e4ca01e8
wwangwe <wangwetymothy@gmail.com>
2023-04-13 14:31:13 +0300

    Use bg image in header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
 import { Fade } from 'react-awesome-reveal'
+import { Link } from 'react-router-dom'
 
 import { Solid } from '../components'
+import { duck } from '../assets'
 
 export const Header = () => {
+    const headerStyle = { backgroundImage: `url(${duck})` }
     return (
-        <header className="relative flex justify-center items-center h-[102vh] -mt-20 bg-gradient-to-b from-secondary to-accent">
+        <header className="relative flex justify-center items-center h-[100vh] md:h-[105vh] -mt-20" style={headerStyle}>
+            <div className="absolute inset-0 bg-gradient-to-b from-secondary to-accent opacity-20 md:opacity-40"></div>
             <div className="relative flex flex-col justify-center items-center space-y-8 md:space-y-10 px-0 md:px-80 text-center">
                 <Fade cascade triggerOnce damping={0.5} direction="up">
-                    <h2 className="text-white uppercase leading-[1.5] text-sm md:text-4xl">Discover the Potential of Your Recommendations</h2>
+                    <h2 className="text-white uppercase leading-[1.5] text-sm md:text-4xl tracking-[3px] md:tracking-[8px]">Discover the Potential of Your Recommendations</h2>
                     <div className="flex justify-between space-x-4">
                         <Solid>
-                            Get Started <i className="fa-solid fa-arrow-right ml-2"></i>
+                            <Link to="/contact/">
+                                Get Started <i className="fa-solid fa-arrow-right ml-2"></i>
+                            </Link>
                         </Solid>
                     </div>
                 </Fade>
